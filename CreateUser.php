d179902b97db475fde92fe11745b8070ea7777e5
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-10-24 21:38:48 -0500

    Got form submission and SQL insert working

--- CreateUser.php
+++ CreateUser.php
@@ -1,17 +1,47 @@
+<!DOCTYPE php>
+<html>
+<head>
+  <link type="text/css" rel="stylesheet" href="style.css" />
+  <title>
+    Create User
+  </title>
+</head>
+<body>
+<header>
+  <nav>
+    <a href="../../index.html">Home</a>
+    &#x2712;
+    <a href="index.html">Lab 05</a>
+  </nav>
+</header>
 <?php
-$mysqli = mysqli_connect("mysql.eecs.ku.edu", "z455a818", jah4uY3u, "z455a818");
-
+$mysqli = new mysqli("mysql.eecs.ku.edu", "z455a818", jah4uY3u, "z455a818");
 if ($mysqli->connect_error) {
-  die("Connection error " . $mysqli->connect_error);
+  die("Connection error " . $mysqli->connect_error . "<br/>");
 }
-
 $username = $_POST["user"];
-$checkQuery = "SELECT user_id FROM Users WHERE user_id IS $username";
-$result = $mysqli->query($query);
-if (!$result) {
-  $query = "INSERT INTO Users ({$_POST["user"]});";
-  $mysqli->mysqli_query($query);
+$query = "INSERT INTO Users (user_id) VALUES ('$username');";
+if ($mysqli->query($query) === TRUE) {
+  echo "Added $username to Users table" . "<br/>";
 }
-
+else {
+  echo "$username already in table" . "<br/>";
+}
+$query = "SELECT user_id FROM Users";
+$result = $mysqli->query($query);
+if ($result->num_rows > 0) {
+  echo "<table><tr><th>ID</th></tr>";
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+      echo "<tr><td>".$row["user_id"]."</td></tr>";
+  }
+  echo "</table>";
+} else {
+  echo "0 results" . "<br/>";
+}
+echo "Made it to end" . "<br/>";
+$mysqli->close();
 
 ?>
+<body>
+<html>
