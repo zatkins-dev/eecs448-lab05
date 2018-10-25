2181599a87dad4e9634ba4b90809e28dd89af5f3
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-10-25 14:32:15 -0500

    set up basic backend

---
+++ CreatePosts.php
@@ -0,0 +1,54 @@
+<!DOCTYPE php>
+<html>
+<head>
+  <link type="text/css" rel="stylesheet" href="style.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>
+    Create New Post
+  </title>
+</head>
+<body>
+<header>
+  <nav>
+    <a href="../../index.html">Home</a>
+    &#x2712;
+    <a href="index.html">Lab 05</a>
+    &#x2712;
+    <a href="CreatePosts.html">Create New Post</a>
+  </nav>
+</header>
+<main class="main">
+<?php
+include_once("ViewPost.php");
+include_once("mysqliConnection.php");
+$mysqli = sqlInit();
+$username = $_POST["user"];
+$usernameQuery = "SELECT user_id FROM Users WHERE (user_id='$username');";
+if ($mysqli->query($usernameQuery) === TRUE) {
+  echo "$username in table" . "<br/>";
+}
+else {
+  die("Username '$username' not found: " . $mysqli->error);
+}
+$postsQuery = "SELECT * FROM Posts;";
+$result = $mysqli->query($postsQuery);
+$postId = $result->num_rows;
+$post = $_POST['post'];
+if ($post === "") {
+    die("New post must contain content.");
+}
+$query = "INSERT INTO Posts (post_id, content, author_id) VALUES ('$postId','$post','$username')";
+$result = $mysqli->query($query);
+if (!$result) {
+    die("Post Submission Error: Post not submitted <br>" . $mysqli->error);
+}
+else {
+    echo "Successfully added new post." . "<br/>";
+    echo displayPost($postId);
+}
+$mysqli->close();
+?>
+</main>
+<body>
+<html>
